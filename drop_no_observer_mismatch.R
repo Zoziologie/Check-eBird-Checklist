2ddf126cc3db8e2469a63667f7fd847cbe3c9e3c
Linus Blomqvist <linus@ucsb.edu>
2025-04-17 13:12:00 -0700

    Some updates

--- drop_no_observer_mismatch.R
+++ drop_no_observer_mismatch.R
@@ -2,9 +2,9 @@
 
 library(tidyverse)
 
-check_df <- read_csv("~/Library/CloudStorage/Dropbox/Birding/ebird_R/outputs/chk_files/check_ebird_checklist_oct_2024_v2.csv")
+check_df <- read_csv("~/Library/CloudStorage/Dropbox/Birding/ebird_R/outputs/chk_files/check_ebird_checklist_feb_2025.csv")
 
 check_clean <- check_df %>%
-  filter(!(flags == "no_observer_mismatch" & str_detect(observer_id, "obsr729081|obsr4754607")))
+  filter(!(flags == "no_observer_mismatch" & str_detect(observer_id, "obsr729081|obsr4754607|obsr656332|obsr1198534|obsr190329|obsr2070027|obsr4916683|obsr4912817|obsr4584609")))
 
-write_csv(check_clean, "~/Library/CloudStorage/Dropbox/Birding/ebird_R/outputs/chk_files/check_ebird_checklist_oct_2024_v3.csv")
+write_csv(check_clean, "~/Library/CloudStorage/Dropbox/Birding/ebird_R/outputs/chk_files/drop_obs_mismatch/check_ebird_checklist_feb_2025.csv")

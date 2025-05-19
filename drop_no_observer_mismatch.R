88f8f1d81a98cb422cfc9241597e43da28c41b68
Linus Blomqvist <linus@ucsb.edu>
2025-05-19 10:52:22 -0700

    Updated drop observer script

--- drop_no_observer_mismatch.R
+++ drop_no_observer_mismatch.R
@@ -1,10 +1,18 @@
 # Code to filter out specific observer IDs for check no_observer_mismatch
 
+# Load packages
 library(tidyverse)
 
-check_df <- read_csv("~/Library/CloudStorage/Dropbox/Birding/ebird_R/outputs/chk_files/check_ebird_checklist_feb_2025.csv")
+# Set parameters
+date <- "april_2025"
+
+id_drop <- "obsr729081|obsr4754607|obsr656332|obsr1198534|obsr190329|obsr2070027|obsr4916683|obsr4912817|obsr4584609"
+
+# Execute
+check_df <- read_csv(str_c("~/Library/CloudStorage/Dropbox/Birding/ebird_R/outputs/chk_files/check_ebird_checklist_", date, ".csv", sep = ""))
 
 check_clean <- check_df %>%
-  filter(!(flags == "no_observer_mismatch" & str_detect(observer_id, "obsr729081|obsr4754607|obsr656332|obsr1198534|obsr190329|obsr2070027|obsr4916683|obsr4912817|obsr4584609")))
+  filter(!(flags == "no_observer_mismatch" & str_detect(observer_id, id_drop)))
 
-write_csv(check_clean, "~/Library/CloudStorage/Dropbox/Birding/ebird_R/outputs/chk_files/drop_obs_mismatch/check_ebird_checklist_feb_2025.csv")
+# Save
+write_csv(check_clean, str_c("~/Library/CloudStorage/Dropbox/Birding/ebird_R/outputs/chk_files/drop_obs_mismatch/check_ebird_checklist_", date, ".csv", sep = ""))
